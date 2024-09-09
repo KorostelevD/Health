f8b2b52e9a9c7fe265be1c0098634cbb9ea808ed
Dmytro Korostelyov <m0501543116@gmail.com>
2024-09-09 19:17:00 +0300

    Init

---
+++ src/main/java/com/example/Health/HealthApplication.java
@@ -0,0 +1,22 @@
+package com.example.Health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+
+	}
+
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void applicationReady(){
+		System.err.println("Started");
+	}
+
+}
---
+++ src/main/java/com/example/Health/models/Patient.java
@@ -0,0 +1,41 @@
+package com.example.Health.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//
+@Entity
+@Table(name = "patients")
+@ToString
+
+public class Patient {
+    public enum Gender{
+        NONE,
+        MALE,
+        FEMALE
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    // Поля пацієнта
+    @Column(length = 50, nullable = false)
+    private String surname;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 50, nullable = false)
+    private String patronymic;
+    @Column(nullable = false, columnDefinition = "int(1) default 0")
+    private Gender gender;
+    @Column(nullable = false)
+    private Date birthDate;
+    @Column(nullable = false)
+    private String phonenumber;
+    @Column(nullable = false)
+    private String email;
+}
