b7847eb9876c6cdf220bc4d1821555887be075fc
Dmytro Korostelyov <m0501543116@gmail.com>
2024-09-12 20:21:44 +0300

    Init3

---
+++ src/main/java/com/example/Health/models/Doctor.java
@@ -0,0 +1,34 @@
+package com.example.Health.models;
+
+import jakarta.persistence.*;
+import lombok.*; //імпорт бібліотеки Lombok, яка автоматично генерує такі методи, як getter, setter, toString, конструктори тощо, з відповідними анотаціями.
+
+@Getter
+@Setter
+@NoArgsConstructor //Генерує конструктор без аргументів. Це означає, що при створенні об'єкта класу можна буде створити його без передавання жодних значень у конструктор.
+@AllArgsConstructor //Генерує конструктор, який приймає аргументи для всіх полів класу. Це означає, що при створенні об'єкта класу можна буде передати значення для всіх його атрибутів.
+
+@Table(name = "doctors")
+@ToString //@ToString є частиною бібліотеки Lombok і автоматично генерує метод toString() для класу. Метод toString() відповідає за повернення текстового представлення об'єкта класу, що включає значення його полів.
+@EqualsAndHashCode
+
+@Entity //Ця анотація позначає клас як сутність для JPA, що означає, що його екземпляри будуть зберігатися в базі даних.
+
+
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String speciality;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 50, nullable = false)
+    private String surname;
+    @Column(nullable = false)
+    private String contact;
+
+
+
+}
---
+++ src/main/java/com/example/Health/models/Payment.java
@@ -0,0 +1,28 @@
+package com.example.Health.models;
+
+import jakarta.persistence.*;
+import lombok.*; //імпорт бібліотеки Lombok, яка автоматично генерує такі методи, як getter, setter, toString, конструктори тощо, з відповідними анотаціями.
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor //Генерує конструктор без аргументів. Це означає, що при створенні об'єкта класу можна буде створити його без передавання жодних значень у конструктор.
+@AllArgsConstructor //Генерує конструктор, який приймає аргументи для всіх полів класу. Це означає, що при створенні об'єкта класу можна буде передати значення для всіх його атрибутів.
+
+@Table(name = "paymest")
+@ToString //@ToString є частиною бібліотеки Lombok і автоматично генерує метод toString() для класу. Метод toString() відповідає за повернення текстового представлення об'єкта класу, що включає значення його полів.
+@EqualsAndHashCode
+
+@Entity //Ця анотація позначає клас як сутність для JPA, що означає, що його екземпляри будуть зберігатися в базі даних.
+
+
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private Date recordDate;
+    @Column(nullable = false)
+    private boolean payment;
+}
